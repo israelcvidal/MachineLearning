4b808314880380fa35e137bc8666e2a93f84b310
Israel Vidal <israelcvidal@gmail.com>
2017-10-18 13:15:56 -0300

    Fixing a few bugs on implementation

--- LogisticRegretion/logistic_regretion.py
+++ LogisticRegretion/logistic_regretion.py
@@ -11,17 +11,23 @@ def sigmoid(scores):
     return 1 / (1 + np.exp(-scores))
 
 
-def logistic_predict(x, coefficients):
+def logistic_predict(x, coefficients, label=False):
     y_predicted = coefficients[0]
     for i in range(len(x)):
         y_predicted += x[i] * coefficients[i+1]
-
+    if label:
+        predicted = sigmoid(y_predicted)
+        if predicted >= 0.5:
+            return 1
+        else:
+            return 0
     return sigmoid(y_predicted)
 
 
 def sgd(train_, alpha_, epochs_):
     error_history = []
-    w_ = [random.random() for _ in range(len(train_[0]))]
+    # w_ = [random.random() for _ in range(len(train_[0]))]
+    w_ = [0 for _ in range(len(train_[0]))]
 
     for epoch in range(epochs_):
         train_ = shuffle(train_)
@@ -34,7 +40,7 @@ def sgd(train_, alpha_, epochs_):
             for i in range(len(row) - 1):
                 w_[i + 1] += (alpha_ * error * row[i])
 
-        error_history.append(sum(epoch_error)/(2*len(epoch_error)))
+        error_history.append(sum(epoch_error)/len(epoch_error))
     return w_, error_history
 
 
@@ -49,7 +55,7 @@ def least_squares(X, y):
 
 def sgd_regularized(train_, alpha_, epochs_, lambda_):
     error_history = []
-    w_ = [random.random() for _ in range(len(train_[0]))]
+    w_ = [0 for _ in range(len(train_[0]))]
 
     for epoch in range(epochs_):
         train_ = shuffle(train_)
@@ -62,7 +68,7 @@ def sgd_regularized(train_, alpha_, epochs_, lambda_):
             for i in range(len(row) - 1):
                 w_[i + 1] += alpha_ * ((error * row[i]) - lambda_*w_[i+1])
 
-        error_history.append(sum(epoch_error)/(2*len(epoch_error)))
+        error_history.append(sum(epoch_error)/len(epoch_error))
     return w_, error_history
 
 
@@ -70,7 +76,7 @@ def mse(real, predicted):
     error_ = 0
     for i, j in zip(real, predicted):
         error_ += (i-j)**2
-    return error_/(2*len(real))
+    return error_/len(real)
 
 
 def k_fold(data_, alpha_, epochs_, k):
@@ -86,8 +92,8 @@ def k_fold(data_, alpha_, epochs_, k):
             train_.extend(k)
 
         w_, _ = sgd(train_, alpha_, epochs_)
-        predicted_test = [logistic_predict(x, w_) for x in test_[:, :-1]]
-        test_error = mse(test_[:, -1], predicted_test)
+        predicted_test = [logistic_predict(x, w_, label=True) for x in test_[:, :-1]]
+        test_error = sum(abs(test_[:, -1] - predicted_test))/len(predicted_test)
         ws_.append(w_)
         error_.append(test_error)
 
@@ -116,44 +122,54 @@ def plot_epochs_error(epochs_, epoch_errors, save=None):
     plt.close()
 
 
-if __name__ == '__main__':
+def question_1():
     data_1 = np.loadtxt("data/ex2data1.txt", delimiter=",")
 
     # # plot dataset1:
     # plot_data(data_1)
 
-    normalize(data_1[:-1], axis=0, copy=False)
-    train = data_1[:int(len(data_1) * 0.7)]
-    test = data_1[int(len(data_1) * 0.7):]
+    normalize(data_1[:, :-1], axis=0, copy=False)
+    train = data_1[:int(len(data_1) * 0.7), :]
+    test = data_1[int(len(data_1) * 0.7):, :]
     alpha = 0.01
     epochs = 1000
 
     # # run logistic refression for dataset1:
-    # w, epoch_errors = sgd(train_=data_1, alpha_=alpha, epochs_=epochs)
-    #
-    # # show results for dataset1:
-    # print("w: " + str(w))
-    # plot_epochs_error(epochs, epoch_errors, save=False):
-    # predicted_test = [logistic_predict(x, w) for x in test[:, :-1]]
-    # test_error = mse(test[:, -1], predicted_test)
-    # print("test error: ", test_error)
-    # ws, error = k_fold(data_1, alpha_=alpha, epochs_=epochs, k=5)
-    # for i, w in enumerate(ws):
-    #     print("w"+str(i)+": ", w)
-    # print(error)
+    w, epoch_errors = sgd(train_=train, alpha_=alpha, epochs_=epochs)
 
-    #################################################
+    # show results for dataset1:
+    print("w: " + str(w))
+    plot_epochs_error(epochs, epoch_errors, save="epochs_mse_dataset1")
+    predicted_test = [logistic_predict(x, w, label=True) for x in test[:, :-1]]
+    test_error = sum(abs(test[:, -1] - predicted_test))
+    test_error_percent = test_error / len(predicted_test)
+    print("test error: ", test_error, test_error_percent)
 
+    print("\nKFOLD:")
+    ws, error = k_fold(data_1, alpha_=alpha, epochs_=epochs, k=5)
+    for i, w in enumerate(ws):
+        print("w" + str(i) + ": ", w)
+    print(error)
+
+
+def question_2():
     data_2 = np.loadtxt("data/dataset_mapfeature.txt", delimiter=",")
     # plot_data(data_2)
 
-    # normalize(data_2[:-1], axis=0, copy=False)
-    # train = data_2[:int(len(data_2) * 0.7)]
-    # test = data_2[int(len(data_2) * 0.7):]
-    # alpha = 0.01
-    # epochs = 1000
-
-    mse_dataset = []
+    normalize(data_2[:, :-1], axis=0, copy=False)
+    alpha = 0.01
+    epochs = 1000
     for lambda_ in [0, 0.01, 0.25]:
         W, _ = sgd_regularized(data_2[:, :-1], alpha, epochs, lambda_)
         print(W)
+
+
+def main():
+    question_1()
+    question_2()
+
+    #################################################
+
+
+if __name__ == '__main__':
+    main()
