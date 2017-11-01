aa5f10c7293eae5e17d321cce5724018fd7259d0
israelcvidal <israelcvidal@gmail.com>
2017-10-31 21:45:09 -0300

    changing all to matrix view

--- MLP/mlp.py
+++ MLP/mlp.py
@@ -27,56 +27,55 @@ class NeuralNetwork(object):
         self.output_nodes = n_output
         self.learning_rate = learning_rate
         self.n_epochs = n_epochs
-        # wij = peso do neuronio i da camada j
         self.wih = np.random.rand(n_hidden, n_input)
         self.who = np.random.rand(n_output, n_hidden)
 
-    def fit(self, train_):
-        train_ = np.concatenate(([[-1] for _ in range(len(train_))], train_))
+    def fit(self, x_train, y_train):
+        x_train = np.concatenate(([[-1] for _ in range(len(x_train))], x_train))
         error_history = []
 
         for epoch in range(self.n_epochs):
-            train_ = shuffle(train_)
+            x_train, y_train = shuffle(x_train, y_train)
 
             epoch_error = []
-            for row in train_:
-                # The input to the hidden layer is the weights (wih) multiplied by inputs
-                hidden_inputs = np.dot(self.wih, row[:-1])
-                # The outputs of the hidden layer pass through sigmoid activation function
-                hidden_outputs = sigmoid(hidden_inputs)
-                # The input to the output layer is the weights (who) multiplied by hidden layer
-                output_inputs = np.dot(self.who, hidden_outputs)
-                #  The output of the network passes through sigmoid activation function
-                outputs = sigmoid(output_inputs)
 
-                # Error is TARGET - OUTPUT
-                error = row[-1] - outputs
+            # The input to the hidden layer is the weights (wih) multiplied by inputs
+            hidden_inputs = np.dot(self.wih, train_[:, :-1])
+            # The outputs of the hidden layer pass through sigmoid activation function
+            hidden_outputs = sigmoid(hidden_inputs)
+            # The input to the output layer is the weights (who) multiplied by hidden layer
+            output_inputs = np.dot(self.who, hidden_outputs)
+            #  The output of the network passes through sigmoid activation function
+            outputs = sigmoid(output_inputs)
 
-                epoch_error.append(error ** 2)
+            # Error is TARGET - OUTPUT
+            errors = y_train - outputs
 
-                # Now we are starting back propogation!
+            epoch_error.append(errors ** 2)
 
-                # Transpose hidden <-> output weights
-                who_t = self.who.transpose()
-                # Hidden errors is output error multiplied by weights (who)
-                hidden_errors = np.dot(who_t, error)
+            # Now we are starting back propogation!
 
-                # Calculate the gradient, this is much nicer in python!
-                gradient_output = d_sigmoid(outputs)*error
+            # Transpose hidden <-> output weights
+            who_t = self.who.transpose()
+            # Hidden errors is output errors multiplied by weights (who)
+            hidden_errors = np.dot(who_t, errors)
 
-                # Gradients for next layer, more back propogation!
-                # Weight by errors and learning rate
-                gradient_hidden = d_sigmoid(hidden_outputs)*hidden_errors*self.learning_rate
+            # Calculate the gradient, this is much nicer in python!
+            gradient_output = d_sigmoid(outputs)*errors
 
-                # // Change in weights from HIDDEN --> OUTPUT
-                hidden_outputs_t = hidden_outputs.transpose()
-                delta_w_output = np.dot(gradient_output, hidden_outputs_t)
-                self.who = self.who + delta_w_output
+            # Gradients for next layer, more back propogation!
+            # Weight by errors and learning rate
+            gradient_hidden = d_sigmoid(hidden_outputs)*hidden_errors*self.learning_rate
 
-                # // Change in weights from INPUT --> HIDDEN
-                inputs_t = row.transpose()
-                delta_w_hidden = np.dot(gradient_hidden, inputs_t)
-                self.wih = self.wih + delta_w_hidden
+            # // Change in weights from HIDDEN --> OUTPUT
+            hidden_outputs_t = hidden_outputs.transpose()
+            delta_w_output = np.dot(gradient_output, hidden_outputs_t)
+            self.who = self.who + delta_w_output
+
+            # // Change in weights from INPUT --> HIDDEN
+            inputs_t = x_train.transpose()
+            delta_w_hidden = np.dot(gradient_hidden, inputs_t)
+            self.wih = self.wih + delta_w_hidden
 
             error_history.append(sum(epoch_error) / len(epoch_error))
         return self.wih, self.who, error_history
